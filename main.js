0af55617ee124e97840a1d3def3e47e51d7e222e
Paulo Fróes <paulo.froes@eduliv.com.br>
2023-12-09 18:10:55 -0300

    Aula 2 sobre funções com desafio

---
+++ main.js
@@ -0,0 +1,15 @@
+const num1 = 10
+const num2 = 100
+
+function multiplicaDoisNumeros(num1, num2){
+    const resultadoMult = num1 * num2
+
+    return resultadoMult
+}
+
+
+const mult = multiplicaDoisNumeros(100, 50)
+console.log('O resultado é: ', mult)
+
+
+
